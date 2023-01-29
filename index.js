5fdf8d7a8e62334c21d10174905872f34967182b
Annu Kashyap <kashyapannu933@gmail.com>
2023-01-29 16:54:58 +0530

    https://github.com/annu933/digital-watch.git

---
+++ index.js
@@ -0,0 +1,34 @@
+const hourEI = document.getElementById("hour")
+const minuteEI = document.getElementById("minutes")
+const secondEI = document.getElementById("seconds")
+const ampmEI = document.getElementById("ampm")
+
+function updateclock(){
+    let h = new Date().getHours()
+    console.log(h)
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
+
+    if ( h > 12 ){
+        h = h - 12;
+        ampm = "PM"
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+
+
+
+
+    hourEI.innerText = h;
+    minuteEI.innerText = m;
+    secondEI.innerText = s;
+    ampmEI.innerText = ampm;
+    setTimeout(()=>{
+        updateclock()
+    },1000)
+}
+updateclock();
